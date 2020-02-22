a3cc15de6de96a16d2ec5f371f87de4caa94d196
Oscar <oma_1506@hotmail.com>
2020-02-22 15:56:27 -0600

    Guardando articulos en DB

--- Backend/controllers/article.js
+++ Backend/controllers/article.js
@@ -1,5 +1,10 @@
 'use strict'
 
+//importaciones
+//Para validar cualquier cosa
+var validator = require('validator');
+var Article = require('../models/article');
+
 var controller = {
     datosCurso: (req, res) => {
         //Devolver datos como JSON
@@ -13,6 +18,64 @@ var controller = {
         return res.status(200).send({
             message: 'Soy la accion test de mi controlador de articulos'
         })
+    },
+
+    save: (req, res) => {
+        //Recoger parametros por post
+        var params = req.body;
+
+
+        //Validar datos (validator)
+        try {
+            //variable verdadera cuando no  este vacio params tittle y content
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_content = !validator.isEmpty(params.content);
+        } catch (err) {
+            return res.status(200).send({
+                status: 'error',
+                message: 'Faltan datos por enviar!!!'
+            });
+        }
+
+        if (validate_title && validate_content) {
+            //Crear el objeto a guardar
+            //intancia del modelo
+            var article = new Article();
+
+            //Asignar valores
+            article.title = params.title;
+            article.content = params.content;
+            article.image = null;
+
+            //Guardar el articulo
+            article.save((err, articleStored) => {
+
+                if (err || !articleStored) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'No se ha guardado el articulo!!!'
+                    });
+                }
+                return res.status(200).send({
+                    status: 'success',
+                    article: articleStored
+                });
+
+            });
+            //Devolver respuesta
+
+            //prueba
+            return res.status(200).send({
+                status: 'success',
+                article
+            });
+        } else {
+            return res.status(200).send({
+                status: 'error',
+                message: 'Los datos no son validos!!!'
+            });
+        }
+
     }
 
 }; //end controller
--- Backend/routes/article.js
+++ Backend/routes/article.js
@@ -6,8 +6,13 @@ var ArticleController = require('../controllers/article');
 //llamar al router
 
 var router = express.Router();
+//Rutas de prueba
 //Crear ruta por get
 router.get('/datos-curso', ArticleController.datosCurso);
 router.get('/test-de-controllador', ArticleController.test);
+
+//Rutas  utiles
+router.post('/save', ArticleController.save);
+
 
 module.exports = router;
