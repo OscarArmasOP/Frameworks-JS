bf43195d691a3d917d31c3ae73effd7bf6b456b3
Oscar <oma_1506@hotmail.com>
2020-02-24 19:14:32 -0600

    Operaciones básicas para el Backend

--- Backend/controllers/article.js
+++ Backend/controllers/article.js
@@ -76,8 +76,146 @@ var controller = {
             });
         }
 
-    }
+    },
 
+    getArticles: (req, res) => {
+
+        var query = Article.find({});
+
+        var last = req.params.last;
+        if (last || last != undefined) {
+            query.limit(5);
+        }
+
+
+        //Find pa sacar datos
+        query.sort('-_id').exec((err, articles) => {
+            if (err) {
+                return res.status(200).send({
+                    status: 'error',
+                    message: 'Error al devolver los articulos'
+                });
+            }
+            if (!articles) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay articulos para mostrar'
+                });
+            }
+            return res.status(200).send({
+                status: 'succes',
+                articles
+            });
+        });
+
+    },
+
+    getArticle: (req, res) => {
+        //Recoger el id de la url
+        var articleId = req.params.id;
+
+        //Comprobar qe existe
+        if (!articleId || articleId == null) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'No existe el articulo'
+            });
+        }
+        //Buscar el articulo
+        Article.findById(articleId, (err, article) => {
+            if (!article || err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al devolver los datos!!!'
+                });
+            }
+
+            //Devolver json
+            return res.status(200).send({
+                status: 'succes',
+                article
+            });
+
+        });
+    },
+
+    update: (req, res) => {
+        //Recoger el id del articulo por la url
+        var articleId = req.params.id;
+        //recoger los datos que llegan por put 
+        var params = req.body;
+        //Validar los datos
+        try {
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_content = !validator.isEmpty(params.content);
+
+        } catch (err) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'Faltan datos por enviar'
+            });
+        }
+
+        if (validate_title && validate_content) {
+            //Find and update
+            Article.findOneAndUpdate({ _id: articleId }, params, { new: true }, (err, articleUpdated) => {
+                if (err) {
+                    return res.status(500).send({
+                        status: 'error',
+                        message: 'Error al actualizar!!!'
+                    });
+                }
+
+                if (!articleUpdated) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'No existe el articuloo'
+                    });
+                }
+
+                return res.status(200).send({
+                    status: 'success',
+                    article: articleUpdated
+                });
+
+            });
+
+        } else {
+            //Devolver respuesta
+            return res.status(200).send({
+                status: 'error',
+                message: 'La validación no es correcta'
+            });
+        }
+    },
+
+    delete: (req, res) => {
+        //Recoger el id de la url
+        var articleId = req.params.id;
+
+        //Find and delete
+        Article.findOneAndDelete({ _id: articleId }, (err, articleRemoved) => {
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al borrar'
+                });
+            }
+            if (!articleRemoved) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No se ha borrado el articulo, posiblemente no exista!!!'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'succes',
+                article: articleRemoved
+            });
+        });
+
+
+    }
 }; //end controller
 
 module.exports = controller;
